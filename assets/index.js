a3de1bdf5e52f3ec014f8869d7e9091ff0d6a35c
Santiago G <sgonzalezhernandez081@gmail.com>
2022-01-22 22:05:25 -0500

    feat(Autoplay): added plugin Autoplay

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,10 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/Autoplay.js';
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [
+    new AutoPlay()
+] });
 
-const button = document.querySelector('button');
+const button = document.querySelector('#btnPlay');
 button.onclick = () => player.togglePlay();
