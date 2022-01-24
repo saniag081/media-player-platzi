4470fca93ef0922fac3fabd9f951f344e9627b0f
Santiago G <sgonzalezhernandez081@gmail.com>
2022-01-24 17:29:33 -0500

    feat(this.html): added this

--- assets/index.js
+++ assets/index.js
@@ -3,7 +3,7 @@ import AutoPlay from './plugins/Autoplay.js';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({ el: video, plugins: [
-    new AutoPlay()
+    //new AutoPlay()
 ] });
 
 const buttonPlay = document.querySelector('#btnTogglePlay');
