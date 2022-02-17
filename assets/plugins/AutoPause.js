ef11864919d15377c8ede155d1ccffd05d2d99be
Santiago G <sgonzalez@modyo.com>
2022-02-16 21:26:57 -0500

    feat(AutoPause.js): added event visibilitychange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -1,19 +1,22 @@
 class AutoPause {
     constructor(){
-        this.threshold = 0.25;
-        this.handlerIntersection = this.handlerIntersection.bind(this);
+        this.threshold = 0.50;
+        this.handleIntersection = this.handleIntersection.bind(this);
+        this.handleVisibilyChange = this.handleVisibilyChange.bind(this);
     }
 
     run(player) {
         this.player = player;
-        const observer = new IntersectionObserver(this.handlerIntersection, { 
+        const observer = new IntersectionObserver(this.handleIntersection, { 
             threshold: this.threshold
         });
 
         observer.observe(player.media);
+
+        document.addEventListener('visibilitychange', this.handleVisibilyChange);
     }
 
-    handlerIntersection(entries) {
+    handleIntersection(entries) {
         const entry = entries[0];
 
         if(entry.intersectionRatio >= this.threshold){
@@ -21,6 +24,16 @@ class AutoPause {
         }else{
             this.player.pause();
         }
+    }
+
+    handleVisibilyChange() {
+        const isVisible = document.visibilityState === 'visible';
+
+        if (isVisible) {
+            this.player.play();
+        } else {
+            this.player.pause();
+        } 
     }
 }
 
